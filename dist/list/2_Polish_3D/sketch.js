09e4678fd14d0e04fcef780ac74c3efa2a0f4e47
Polun <polunwu@gmail.com>
2020-04-08 12:46:24 +0800

    add 2_Polish_3D

---
+++ dist/list/2_Polish_3D/sketch.js
@@ -0,0 +1,21 @@
+let polishman;
+
+function preload(){
+  polishman = loadModel('polish_man.obj', true);
+}
+
+function setup() {
+  createCanvas(600, 600, WEBGL);
+}
+
+function draw() {
+  background(0);
+  scale(1.8);
+  rotateX(radians(180));
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  rotateZ(frameCount * 0.01);
+  normalMaterial();
+  model(polishman);
+
+}
