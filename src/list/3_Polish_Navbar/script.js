4899d8e8ee2eb2ae07e44780b96429b0ea8c113f
Polun <polunwu@gmail.com>
2020-04-16 16:05:42 +0800

    add 3_Polish_Navbar menu-right IN & OUT

--- src/list/3_Polish_Navbar/script.js
+++ src/list/3_Polish_Navbar/script.js
@@ -1,10 +1,12 @@
 window.addEventListener('load', function () {
   const menuTrigger = document.getElementById('menu-trigger');
   const menu = document.getElementById('navigation__menu');
+  const menuRight = document.getElementById('navigation__menu-right');
 
   menuTrigger.addEventListener('click', function () {
     menuTrigger.classList.toggle('menu-trigger--active');
     menu.classList.toggle('navigation__menu--open');
+    menuRight.classList.toggle('navigation__menu-right--is-open');
   })
 
 
