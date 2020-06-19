919a70d4b7800b06510704f7af895606fe4fdc30
polunwu <polunwu@gmail.com>
2020-06-19 19:50:52 +0800

    Modify 2_Polishman random Movement

--- src/list/2_Polish_3D/sketch.js
+++ src/list/2_Polish_3D/sketch.js
@@ -8,20 +8,59 @@ function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
+let SCALE = 4.1;
+
+let env = {
+  x_pos: 40,
+  x_off: 0,
+  y_pos: 50,
+  y_off: 1,
+  z_pos: 0,
+  z_off: 2,
+  angle: 0,
+  speed: 1
+}
+let man = {
+  x: {
+    angle: 180,
+    speed: 0.15
+  },
+  y: {
+    angle: 90,
+    speed: 0.5
+  },
+  z: {
+    angle: 15,
+    speed: 0.15
+  }
+}
+
 function draw() {
   background(0);
-  scale(2.8);
-  rotateY(frameCount * 0.03); // rotate environment
+  scale(SCALE);
+  env.x_off += 0.0025;
+  env.y_off += 0.0025;
+  env.z_off += 0.0025;
+  env.x_pos = noise(env.x_off) * 100;
+  env.y_pos = noise(env.y_off) * 60 + 20;
+  translate(env.x_pos, env.y_pos, env.z_pos);
+  rotateY(radians(env.angle)); // rotate environment
 
   push();
-  rotateX(radians(180));
-  rotateZ(radians(15));
-  rotateY(frameCount * 0.01); // rotate self
-  // rotateX(frameCount * 0.01);
-  // rotateZ(frameCount * 0.01);
+  rotateX(radians(man.x.angle));
+  rotateZ(radians(man.z.angle));
+  rotateY(radians(man.y.angle)); // rotate self
   normalMaterial();
   model(polishman);
   pop();
+
+  if (man.z.angle > 90 || man.z.angle < -90) {
+    man.z.speed = -1 * man.z.speed;
+  }
+
+  env.angle += env.speed;
+  man.y.angle += man.y.speed;
+  man.z.angle += man.z.speed;
   
 }
 
