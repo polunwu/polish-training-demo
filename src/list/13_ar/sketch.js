a51279db49881265cd96818ca718d29a4e537ef2
polunwu <polunwu@gmail.com>
2021-07-08 18:01:20 +0800

    加入 water, box 兩種模型，並且隨機對應一個

--- src/list/13_ar/sketch.js
+++ src/list/13_ar/sketch.js
@@ -1,3 +1,18 @@
+// 隨機生成 0 到 (max-1) 的整數
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 window.addEventListener('load', () => {
-  console.log('load')
+  let model = document.querySelector('#bowser-model');
+  let modelPath = '';
+  
+  if(getRandomInt(2) === 0) { // 0, 1 隨機對應 model 路徑
+    modelPath = "model/untitled/Untitled.gltf"
+  } else {
+    modelPath = "model/water/water.gltf"
+  }
+  
+  console.log(modelPath)
+  model.setAttribute('gltf-model', modelPath)
 });
