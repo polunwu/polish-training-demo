15e12abea58f73d6d9ae3f4fa13310a43de7434c
polunwu <polunwu@gmail.com>
2021-07-08 18:33:11 +0800

    加入模型載入、錯誤事件處理

--- src/list/13_ar/sketch.js
+++ src/list/13_ar/sketch.js
@@ -4,15 +4,29 @@ function getRandomInt(max) {
 }
 
 window.addEventListener('load', () => {
+  let loader = document.querySelector('#loader');
   let model = document.querySelector('#bowser-model');
   let modelPath = '';
+
+  // 模型載入事件
+  model.addEventListener('model-loaded', function () {
+    console.log('model-loaded')
+    setTimeout(() => {
+      loader.classList.add('hide');
+    }, 1000);
+  })
+  model.addEventListener('model-error', function () {
+    console.log('model-loaded');
+    loader.textContent = '錯誤';
+    loader.classList.remove('hide');
+  })
   
+  // 隨機生成模型
   if(getRandomInt(2) === 0) { // 0, 1 隨機對應 model 路徑
     modelPath = "model/untitled/Untitled.gltf"
   } else {
     modelPath = "model/water/water.gltf"
   }
-  
   console.log(modelPath)
   model.setAttribute('gltf-model', modelPath)
 });
